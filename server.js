7704bcbab8379e92209aa1078c356a5f26c3bc4a
falinsky <sergey@falinsky.com>
2017-05-03 22:54:45 +0000

    Implement parse and show date

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-var express = require('express');
+var express = require("express");
+var chrono = require("chrono-node");
+var dateformat = require("dateformat");
+
 var app = express();
+var port = process.argv[2] || 8080;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.get("/:date", function (req, res) {
+  var unixTimestamp = Number(req.params.date);
+      
+  var date = !isNaN(unixTimestamp) ? new Date(unixTimestamp * 1000) : chrono.parseDate(req.params.date);
+  
+  var result = {
+    unix: date ? date.getTime() / 1000 : null,
+    natural: date ? dateformat(date, "mmmm d, yyyy") : null
+  };
+  
+  res.json(result);
+});
+
+app.listen(port, function () {
+  console.log('App listening on port ' + port);
 });
