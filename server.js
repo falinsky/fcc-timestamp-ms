761ad2606a53be056101d1e7fa5d6285ebe7f08a
falinsky <sergey@falinsky.com>
2017-05-04 19:27:40 +0000

    Add detecting port from ENV

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var chrono = require("chrono-node");
 var dateformat = require("dateformat");
 
 var app = express();
-var port = process.argv[2] || 8080;
+var port = process.env.PORT || process.argv[2] || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
